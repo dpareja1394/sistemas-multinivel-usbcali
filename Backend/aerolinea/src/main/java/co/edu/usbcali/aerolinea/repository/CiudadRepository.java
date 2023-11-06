d33cf1e4d3f8a1dd2d6f064d621ca9649e1d4636
Daniel Pareja Londoño <dpareja1394@gmail.com>
2023-11-06 18:29:37 -0500

    Servicio para consultar ciudades

--- Backend/aerolinea/src/main/java/co/edu/usbcali/aerolinea/repository/CiudadRepository.java
+++ Backend/aerolinea/src/main/java/co/edu/usbcali/aerolinea/repository/CiudadRepository.java
@@ -4,6 +4,12 @@ import co.edu.usbcali.aerolinea.domain.Ciudad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CiudadRepository extends JpaRepository<Ciudad, Integer> {
+
+    List<Ciudad> findCiudadsByPaisId(Integer paisId);
+
+    List<Ciudad> findCiudadsByPaisCodigo(String paisCodigo);
 }
