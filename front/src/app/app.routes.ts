b7b95c06c5e04f221a2944792e4c2956a85a78aa
jcabuyales <juancabuyales@gmail.com>
2023-11-12 12:51:38 -0500

    se elimina proyecto base

--- front/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { PaisesComponent } from './paises/paises.component';
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: PaisesComponent
-      },
-];
--- front/src/app/models/pais.ts
+++
@@ -1,6 +0,0 @@
-export interface Pais{
-    id?: number;
-    codigo: string;
-    nombre: string;
-    descripcion: string;
-}
